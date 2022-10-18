ed72e22399612e8b0ce52c40131778977115b854
Bello2288 <bellofattogregg@gmail.com>
2022-10-18 15:54:43 -0400

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,19 @@
+from distutils.command.upload import upload
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+# class Profile(models.Model):
+#     avatar = models.ImageField(upload_to='profiles/') 
+#     user = models.OneToOneField(
+#         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True)
+                                                              
+#     def __str__(self):                                         
+#         return self.user.username  
